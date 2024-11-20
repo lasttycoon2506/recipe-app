746d869338dae1ef0bd712d9323c7a01a1a0dbe0
William Roberts <werobert@yahoo.com>
2024-11-20 13:13:24 -0500

    login dto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -30,6 +30,9 @@ public class AccountController(DataContext context) : BaseApiController
         return newUser;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<User>> Login() { }
+
     public async Task<bool> UsernameExists(string username)
     {
         return await context.Users.AnyAsync(user => user.UserName == username.ToLower());
