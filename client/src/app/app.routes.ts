cea9db00ae18ec8abd148628f1b18935cd31b32e
William Roberts <werobert@yahoo.com>
2024-12-01 16:54:26 -0500

    route-guards impl.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,19 @@ import { MessagesComponent } from './components/messages/messages.component';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
 import { HomeComponent } from './components/home/home.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'recipes', component: RecipesListComponent },
-  { path: 'recipes/:id', component: RecipeDetailComponent },
+  { path: 'messages', component: MessagesComponent, canActivate: [authGuard] },
+  {
+    path: 'recipes',
+    component: RecipesListComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'recipes/:id',
+    component: RecipeDetailComponent,
+    canActivate: [authGuard],
+  },
 ];
