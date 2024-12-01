12cc661659151bc76b9e2e3cbff62f31f8f6523f
William Roberts <werobert@yahoo.com>
2024-12-01 13:44:46 -0500

    routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: 'recipes', component: RecipesListComponent },
+  { path: 'recipes/:id', component: RecipeDetailComponent },
+];
