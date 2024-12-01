4653164295ab04eb98f89e8d4b38404b60fe7897
William Roberts <werobert@yahoo.com>
2024-12-01 13:17:10 -0500

    folder renaming

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
-import { AccountService } from './services/account.service';
+import { AccountService } from './_services/account.service';
 import { HomeComponent } from './home/home.component';
 
 @Component({
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AccountService } from '../services/account.service';
+import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
