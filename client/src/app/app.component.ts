4be7685827e7465e22f57dc5f1b8ea694c2ace78
William Roberts <werobert@yahoo.com>
2024-12-01 14:29:47 -0500

    removed home-comp. from app-comp

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,12 +3,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
