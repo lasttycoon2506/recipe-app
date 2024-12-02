0f03975b96dd141871d01f66f2561365cb253ee8
William Roberts <werobert@yahoo.com>
2024-12-02 12:36:47 -0500

    res depending on env

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,14 +1,29 @@
+using System.Net;
+using API.Errors;
+
 namespace API.Middleware;
 
 public class ExceptionMiddleware(
     RequestDelegate next,
     ILogger<ExceptionMiddleware> logger,
     IHostEnvironment env
-) { 
-    try {
-        
-    }
-    catch (Exception ex) {
+)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "response.json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
+            var response = env.IsDevelopment()
+                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                : new ApiException(context.Response.StatusCode, ex.Message, "server error");
+        }
     }
 }
