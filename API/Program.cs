a87f5bbf4ee5e5a8dd79cb18175fb34626bf1549
William Roberts <werobert@yahoo.com>
2024-12-02 12:53:16 -0500

    middleware impl.

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,12 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x =>
     x.AllowAnyHeader()
         .AllowAnyMethod()
