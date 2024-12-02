a4816a5de8f97f3613cd65fe199dca624dc6b9b6
William Roberts <werobert@yahoo.com>
2024-12-02 14:11:41 -0500

    user fields added

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -4,6 +4,9 @@ public class User
 {
     public int Id { get; set; }
     public required string UserName { get; set; }
-    public required byte[] PasswordHash { get; set; }
-    public required byte[] PasswordSalt { get; set; }
+    public byte[] PasswordHash { get; set; } = [];
+    public byte[] PasswordSalt { get; set; } = [];
+    public List<Photo> Photos { get; set; } = [];
+    public DateOnly Created { get; set; }
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
 }
