1ec3a18590e4ec7cba3813ac3efd38cc2209d9ad
William Roberts <werobert@yahoo.com>
2024-12-02 16:04:38 -0500

    hash user seed objs

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +12,20 @@ public class Seed
     {
         if (await context.Users.AnyAsync())
             return;
-        var data = File.ReadAllText("UserSeedData.json");
-        var users = JsonSerializer.Deserialize<List<User>>(data);
+        var userData = await File.ReadAllTextAsync("UserSeedData.json");
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var users = JsonSerializer.Deserialize<List<User>>(userData, options);
+
+        if (users == null)
+            return;
+
+        foreach (var user in users)
+        {
+            using var hmac = new HMACSHA512();
+
+            user.UserName = user.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("password"));
+            user.PasswordSalt = hmac.Key;
+        }
     }
 }
