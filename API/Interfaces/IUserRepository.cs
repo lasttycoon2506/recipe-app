c36e170940e624a0a8f8831b7854185d76186699
William Roberts <werobert@yahoo.com>
2024-12-03 13:04:57 -0500

    getUserById optional

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -5,6 +5,6 @@ namespace API.Interfaces;
 public interface IUserRepository
 {
     Task<IEnumerable<User>> GetUsersAsync();
-    Task<User> GetUserAsync(int id);
+    Task<User?> GetUserAsync(int id);
     Task<bool> SaveAsync();
 }
