5b2e769bc02ad8b320dcac74e6973f65a567826c
William Roberts <werobert@yahoo.com>
2024-12-04 11:40:21 -0500

    removed unused interface funcs

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -9,16 +9,6 @@ namespace API.Data;
 
 public class UserRepository(DataContext context, IMapper mapper) : IUserRepository
 {
-    // public async Task<User?> GetUserAsync(int id)
-    // {
-    //     return await context.Users.FindAsync(id);
-    // }
-
-    // public async Task<IEnumerable<User>> GetUsersAsync()
-    // {
-    //     return await context.Users.Include(x => x.Photos).ToListAsync();
-    // }
-
     public async Task<bool> SaveAsync()
     {
         return await context.SaveChangesAsync() > 0;
