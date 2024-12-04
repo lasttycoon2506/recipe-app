5b172ac5fc7c1eedccfdc39231a72714fb1bb95f
William Roberts <werobert@yahoo.com>
2024-12-04 13:17:16 -0500

    member-service token

--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -1,20 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MemberService {
   private http = inject(HttpClient);
+  private accountService = inject(AccountService);
   private baseUrl = environment.apiUrl;
 
   getUser(id: number): Observable<Member> {
-    return this.http.get<Member>(this.baseUrl + 'users/' + id);
+    return this.http.get<Member>(this.baseUrl + 'users/' + id, this.getToken());
   }
 
   getUsers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    return this.http.get<Member[]>(this.baseUrl + 'users', this.getToken());
+  }
+
+  getToken(): {
+    headers: HttpHeaders;
+  } {
+    return {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.accountService.currentUser()?.token,
+      }),
+    };
   }
 }
