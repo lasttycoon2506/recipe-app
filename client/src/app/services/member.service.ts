031474a3703e46b1bab18120bed3aea2e41118d5
William Roberts <werobert@yahoo.com>
2024-12-04 13:05:19 -0500

    getuser/users

--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -1,9 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MemberService {
   private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+
+  getUser(id: number): Observable<Member> {
+    return this.http.get<Member>(this.baseUrl + 'users/' + id);
+  }
+
+  getUsers(): Observable<Member[]> {
+    return this.http.get<Member[]>(this.baseUrl + 'users');
+  }
 }
