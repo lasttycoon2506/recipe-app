b0f43c68938fa3114511d27e1ef137fbf5d164c8
William Roberts <werobert@yahoo.com>
2024-12-05 12:23:41 -0500

    remove jwt from member-service

--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-import { AccountService } from './account.service';
 import { Member } from '../models/member';
 
 @Injectable({
@@ -10,24 +9,13 @@ import { Member } from '../models/member';
 })
 export class MemberService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   private baseUrl = environment.apiUrl;
 
   getMember(id: number): Observable<Member> {
-    return this.http.get<Member>(this.baseUrl + 'users/' + id, this.getToken());
+    return this.http.get<Member>(this.baseUrl + 'users/' + id);
   }
 
   getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getToken());
-  }
-
-  getToken(): {
-    headers: HttpHeaders;
-  } {
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + this.accountService.currentUser()?.token,
-      }),
-    };
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 }
