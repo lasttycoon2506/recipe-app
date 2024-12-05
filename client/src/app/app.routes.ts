a57e4368305d95ecb5e7b3f902af1c23f353c575
William Roberts <werobert@yahoo.com>
2024-12-05 13:14:55 -0500

    recipe-detail renamed

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { MessagesComponent } from './components/messages/messages.component';
-import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { RecipeDetailComponent } from './components/members/member-detail/member-detail.component';
 import { HomeComponent } from './components/home/home.component';
 import { authGuard } from './guards/auth.guard';
 import { MemberListComponent } from './components/members/member-list/member-list.component';
