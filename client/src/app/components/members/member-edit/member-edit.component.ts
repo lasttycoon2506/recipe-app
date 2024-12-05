71ba99345ee2f73d1bdca69d9525a860992a96f5
William Roberts <werobert@yahoo.com>
2024-12-05 16:46:55 -0500

    member-edit component

--- client/src/app/components/members/member-edit/member-edit.component.ts
+++ client/src/app/components/members/member-edit/member-edit.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AccountService } from '../../../services/account.service';
+import { MemberService } from '../../../services/member.service';
 
 @Component({
-  selector: 'app-member-edit',
-  standalone: true,
-  imports: [],
-  templateUrl: './member-edit.component.html',
-  styleUrl: './member-edit.component.css'
+	selector: 'app-member-edit',
+	standalone: true,
+	imports: [],
+	templateUrl: './member-edit.component.html',
+	styleUrl: './member-edit.component.css',
 })
-export class MemberEditComponent {
+export class MemberEditComponent implements OnInit {
+	private accountService = inject(AccountService);
+	private memberService = inject(MemberService);
 
+	ngOnInit(): void {
+		throw new Error('Method not implemented.');
+	}
+
+	loadMember(): void {
+		var user = this.accountService.currentUser();
+		this.memberService.getMember();
+	}
 }
