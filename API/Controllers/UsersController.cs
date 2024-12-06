5c12e986bca946010ef85b39ec8bf5188b6e4cd0
William Roberts <werobert@yahoo.com>
2024-12-06 17:01:59 -0500

    edit-member controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,14 +1,15 @@
-using System.Runtime.CompilerServices;
 using System.Security.Claims;
 using API.DTOs;
+using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -29,10 +30,25 @@ namespace API.Controllers
             return user;
         }
 
-        [HttpPut("{username}")]
-        public async Task<AsyncVoidMethodBuilder> UpdateUser(MemberUpdateDto memberUpdateDto)
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
             var username = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (username == null)
+                return BadRequest("username dne");
+
+            var user = await userRepository.GetUserAsync(username);
+
+            if (user == null)
+                return BadRequest("user dne in db");
+
+            mapper.Map(memberUpdateDto, user);
+
+            if (await userRepository.SaveAsync())
+                return NoContent();
+
+            return BadRequest("failed to update user");
         }
     }
 }
