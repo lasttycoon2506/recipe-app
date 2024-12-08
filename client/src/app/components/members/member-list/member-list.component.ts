87b6415d6f63ee8bc4dea91e1fe64dee51fa6046
William Roberts <werobert@yahoo.com>
2024-12-08 15:37:35 -0500

    init member signal

--- client/src/app/components/members/member-list/member-list.component.ts
+++ client/src/app/components/members/member-list/member-list.component.ts
@@ -11,17 +11,13 @@ import { MemberCardComponent } from '../member-card/member-card.component';
 	styleUrl: './member-list.component.css',
 })
 export class MemberListComponent implements OnInit {
-	private memberService = inject(MemberService);
-	members: Member[] = [];
+	memberService = inject(MemberService);
 
 	ngOnInit(): void {
-		this.loadMembers();
+		if (this.memberService.members().length === 0) this.loadMembers();
 	}
 
 	loadMembers(): void {
-		this.memberService.getMembers().subscribe({
-			next: (members) => (this.members = members),
-			error: (error) => console.log(error),
-		});
+		this.memberService.getMembers();
 	}
 }
