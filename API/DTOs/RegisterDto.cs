c1c3cad2d674820e98a4da48537e00b791c5d73d
William Roberts <werobert@yahoo.com>
2024-12-12 15:21:24 -0500

    registerDto upd.

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,4 +11,10 @@ public class RegisterDto
     [Required]
     [StringLength(16, MinimumLength = 8)]
     public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public string? Experience { get; set; }
+
+    [Required]
+    public string? Specialty { get; set; }
 }
