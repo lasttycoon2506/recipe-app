1fbc5094ac9804c533acbda59561253bc07367e9
William Roberts <werobert@yahoo.com>
2024-12-14 15:28:47 -0500

    userParams helper

---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,15 @@
+namespace API.Helpers;
+
+public class UserParams
+{
+    private const int MaxPgSize = 50;
+    public int PgNumber { get; set; } = 1;
+
+    private int _pgSize = 20;
+
+    public int PageSize
+    {
+        get => _pgSize;
+        set => _pgSize = value > MaxPgSize ? MaxPgSize : value;
+    }
+}
