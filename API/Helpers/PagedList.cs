d40d6e985a52dc497b631e04e50e88c0c8d4853a
William Roberts <werobert@yahoo.com>
2024-12-14 14:04:16 -0500

    pg-list static-meth

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -1,15 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Helpers;
 
 public class PagedList<T> : List<T>
 {
-    public PagedList(IEnumerable<T> items, int count, int pgSize, int pgNumber)
+    public PagedList(IEnumerable<T> items, int totalItemsInDb, int pgSize, int pgNumber)
     {
-        currentPg = pgNumber;
-        totalCount = count;
-        pgs = (int)Math.Ceiling((double)count / pgSize);
+        CurrentPg = pgNumber;
+        TotalItemsInDb = totalItemsInDb;
+        TotalPgs = (int)Math.Ceiling(totalItemsInDb / (double)pgSize);
+        PageSize = pgSize;
+        AddRange(items);
     }
 
-    public int currentPg { get; set; }
-    public int pgs { get; set; }
-    public int totalCount { get; set; }
+    public int CurrentPg { get; set; }
+    public int TotalPgs { get; set; }
+    public int TotalItemsInDb { get; set; }
+    public int PageSize { get; set; }
+
+    public static async Task<PagedList<T>> GetResults(IQueryable<T> query, int pgSize, int pgNumber)
+    {
+        var totalItemsInDb = await query.CountAsync();
+        var items = await query.Skip((pgNumber - 1) * pgSize).Take(pgSize).ToListAsync();
+        return new PagedList<T>(items, totalItemsInDb, pgSize, pgNumber);
+    }
 }
