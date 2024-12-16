15b871f8fe92f6dab7b16af4fbe9f1598a8fa2cc
William Roberts <werobert@yahoo.com>
2024-12-16 11:04:05 -0500

    getMembers res.

--- client/src/app/services/member.service.ts
+++ client/src/app/services/member.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { map, Observable, of, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Member } from '../models/member';
 import { Photo } from '../models/photo';
+import { Pagination, PaginationResult } from '../models/pagination';
 
 @Injectable({
 	providedIn: 'root',
@@ -11,58 +12,67 @@ import { Photo } from '../models/photo';
 export class MemberService {
 	private http = inject(HttpClient);
 	private baseUrl = environment.apiUrl;
-	members = signal<Member[]>([]);
+	// members = signal<Member[]>([]);
+	paginatedMembers = signal<PaginationResult<Member[]> | null>(null);
 
 	getMember(username: string) {
-		const member = this.members().find((m) => m.username === username);
-		if (member) return of(member);
-
-		return this.http.get<Member>(this.baseUrl + 'users/' + username);
+		// const member = this.members().find((m) => m.username === username);
+		// if (member) return of(member);
+		// return this.http.get<Member>(this.baseUrl + 'users/' + username);
 	}
 
 	getMembers(): void {
-		this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
-			next: (members) => this.members.set(members),
-		});
+		let userParams = this.http
+			.get<Member[]>(this.baseUrl + 'users', { observe: 'response' })
+			.subscribe({
+				next: (response) => {
+					this.paginatedMembers.set({
+						items: response.body as Member[],
+						pagination: JSON.parse(
+							response.headers.get('Pagination')!,
+						),
+					});
+				},
+			});
 	}
 
 	updateMember(member: Member): Observable<Response> {
 		return this.http.put<Response>(this.baseUrl + 'users', member);
 	}
 
-	setMainPic(photo: Photo): Observable<Response> {
-		return this.http
-			.put<Response>(this.baseUrl + 'users/set-main-pic/' + photo.id, {})
-			.pipe(
-				tap(() =>
-					this.members.update((members) =>
-						members.map((member) => {
-							if (member.photos.includes(photo)) {
-								member.photoUrl = photo.url;
-							}
-							return member;
-						}),
-					),
-				),
-			);
+	setMainPic(photo: Photo) {
+		// return this.http
+		// 	.put<Response>(this.baseUrl + 'users/set-main-pic/' + photo.id, {})
+		// 	.pipe(
+		// 		tap(() =>
+		// 			this.members.update((members) =>
+		// 				members.map((member) => {
+		// 					if (member.photos.includes(photo)) {
+		// 						member.photoUrl = photo.url;
+		// 					}
+		// 					return member;
+		// 				}),
+		// 			),
+		// 		),
+		// 	);
 	}
 
-	deletePic(photo: Photo): Observable<Response> {
-		return this.http
-			.delete<Response>(this.baseUrl + 'users/delete-pic/' + photo.id)
-			.pipe(
-				tap(() =>
-					this.members.update((members) =>
-						members.map((member) => {
-							if (member.photos.includes(photo)) {
-								member.photos = member.photos.filter(
-									(pic) => pic.id !== photo.id,
-								);
-							}
-							return member;
-						}),
-					),
-				),
-			);
+	deletePic(photo: Photo) {
+		// return this.http
+		// 	.delete<Response>(this.baseUrl + 'users/delete-pic/' + photo.id)
+		// 	.pipe(
+		// 		tap(() =>
+		// 			this.members.update((members) =>
+		// 				members.map((member) => {
+		// 					if (member.photos.includes(photo)) {
+		// 						member.photos = member.photos.filter(
+		// 							(pic) => pic.id !== photo.id,
+		// 						);
+		// 					}
+		// 					return member;
+		// 				}),
+		// 			),
+		// 		),
+		// 	);
 	}
 }
