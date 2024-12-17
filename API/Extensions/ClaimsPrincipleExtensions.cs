3acf361c3a13bcf4ea31fb0a5b86f1b5156c9336
William Roberts <werobert@yahoo.com>
2024-12-17 12:52:38 -0500

    getUserById()

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -7,9 +7,18 @@ public static class ClaimsPrincipleExtensions
     public static string GetUsername(this ClaimsPrincipal user)
     {
         var username =
-            user.FindFirstValue(ClaimTypes.NameIdentifier)
+            user.FindFirstValue(ClaimTypes.Name)
             ?? throw new Exception("username not found in token");
 
         return username;
+    }
+
+    public static string GetUserId(this ClaimsPrincipal user)
+    {
+        var userId =
+            user.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("user-id not found in token");
+
+        return userId;
     }
 }
