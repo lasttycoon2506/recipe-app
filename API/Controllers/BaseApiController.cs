fd1dd5ee740ba297db7b5568226521c2a5b7be7a
William Roberts <werobert@yahoo.com>
2024-12-17 11:29:07 -0500

    activityLog impl.

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,10 @@
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace API.Controllers;
 
 [Route("api/[controller]")]
+[ServiceFilter(typeof(UserActivityLog))]
 [ApiController]
 public class BaseApiController : ControllerBase { }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -22,6 +22,7 @@ public static class ApplicationServiceExtensions
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IPhotoService, PhotoService>();
+        services.AddScoped<UserActivityLog>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
