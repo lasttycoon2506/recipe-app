60c47357a60dd47a5b5954b1e7d30adea2b0e9c6
William Roberts <werobert@yahoo.com>
2024-12-18 13:34:40 -0500

    likes-repo

---
+++ API/Interfaces/ILikesRepo.cs
@@ -0,0 +1,14 @@
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces;
+
+public interface ILikesRepo
+{
+    Task<UserLikes> GetLike(int sourceId, int targetId);
+    Task<IEnumerable<MemberDto>> GetLikes(string predicate, int userId);
+    Task<IEnumerable<int>> GetLikesIds(int currentUserId);
+    void DeleteLike(UserLikes like);
+    void AddLike(UserLikes like);
+    Task<bool> Save();
+}
