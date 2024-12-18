3c597e52bc5e313505000313fe0ecda914e4d794
William Roberts <werobert@yahoo.com>
2024-12-18 13:06:09 -0500

    likes table

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -6,4 +6,25 @@ namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public required DbSet<User> Users { get; set; }
+    public required DbSet<UserLikes> Likes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<UserLikes>().HasKey(key => new { key.SourceUserId, key.TargetUserId });
+
+        builder
+            .Entity<UserLikes>()
+            .HasOne(source => source.SourceUser)
+            .WithMany(target => target.WhoUserLikes)
+            .HasForeignKey(source => source.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .Entity<UserLikes>()
+            .HasOne(source => source.TargetUser)
+            .WithMany(target => target.WhoLikesUser)
+            .HasForeignKey(source => source.TargetUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
