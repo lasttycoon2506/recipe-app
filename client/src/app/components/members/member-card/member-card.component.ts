43e47edf0a4bf28a5354f4273f84fc76d7065a77
William Roberts <werobert@yahoo.com>
2024-12-30 14:35:22 -0500

    removed custom-spinner

--- client/src/app/components/members/member-card/member-card.component.ts
+++ client/src/app/components/members/member-card/member-card.component.ts
@@ -3,22 +3,26 @@ import { Member } from '../../../models/member';
 import { RouterLink } from '@angular/router';
 import { LikesService } from '../../../services/likes.service';
 import { sleep } from '../../../helpers/sleepHelper';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
 	selector: 'app-member-card',
 	standalone: true,
-	imports: [RouterLink],
+	imports: [RouterLink, NgxSpinnerModule],
 	templateUrl: './member-card.component.html',
 	styleUrl: './member-card.component.css',
 })
 export class MemberCardComponent {
 	private likesService = inject(LikesService);
+	private spinnerService = inject(NgxSpinnerService);
 	member = input.required<Member>();
 	reload = output<boolean>();
 
 	async like() {
 		this.likesService.like(this.member().id);
+		this.spinnerService.show();
 		await sleep(); //1 sec - allows db to update and send refreshed members-list
+		this.spinnerService.hide();
 		this.reload.emit(true);
 	}
 }
