26adf96bbfcfdeea0704da9d12764907d93af3dc
William Roberts <werobert@yahoo.com>
2024-12-30 15:12:56 -0500

    message entity

---
+++ API/Entities/Message.cs
@@ -0,0 +1,13 @@
+namespace API.Entities;
+
+public class Message
+{
+    public required string SenderUsername { get; set; }
+    public required string ReceiverUsername { get; set; }
+    public required string Content { get; set; }
+    public DateTime MessageSent { get; set; } = DateTime.UtcNow;
+    public int SenderId { get; set; }
+    public User Sender { get; set; } = null!;
+    public int ReceiverId { get; set; }
+    public User Receiver { get; set; } = null!;
+}
