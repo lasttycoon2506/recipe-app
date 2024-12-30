de0ff61ba08a0f2dca6fb00862ddf8419dc09d4c
William Roberts <werobert@yahoo.com>
2024-12-30 16:35:13 -0500

    messageDTO

---
+++ API/DTOs/MessageDto.cs
@@ -0,0 +1,14 @@
+namespace API.DTOs;
+
+public class MessageDto
+{
+    public int Id { get; set; }
+    public required string SenderUsername { get; set; }
+    public int SenderId { get; set; }
+    public required string SenderPicUrl { get; set; }
+    public required string ReceiverUsername { get; set; }
+    public int ReceiverId { get; set; }
+    public required string ReceiverPicUrl { get; set; }
+    public required string Content { get; set; }
+    public DateTime TimeSent { get; set; }
+}
