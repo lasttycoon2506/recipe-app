68811fd7d4de5f37b08aeef45b8e68c610d4b023
William Roberts <werobert@yahoo.com>
2024-12-30 13:56:50 -0500

    loadingService

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { jwtInterceptor } from './interceptors/jwt.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -12,5 +13,8 @@ export const appConfig: ApplicationConfig = {
 		provideHttpClient(withInterceptors([jwtInterceptor])),
 		provideAnimations(),
 		provideToastr({ positionClass: 'toast-bottom-right' }),
+		importProvidersFrom(
+			NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
+		),
 	],
 };
