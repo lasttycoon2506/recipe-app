76c6cee32edac0a2071f62b5f42dcd39c87c375d
William Roberts <werobert@yahoo.com>
2024-12-31 11:12:28 -0500

    createMessage mappings

--- API/Controllers/MessageController.cs
+++ API/Controllers/MessageController.cs
@@ -18,14 +18,24 @@ namespace API.Controllers
         {
             var username = User.GetUsername();
             if (username == createMessageDto.ReceiverUsername)
-                return BadRequest("cant send message to self!");
+                return BadRequest("cant message self!");
+
+            var sender = await userRepository.GetUserAsync(username);
+            var receiver = await userRepository.GetUserAsync(createMessageDto.ReceiverUsername);
+            if (sender == null || receiver == null)
+                return NotFound("sender or receiver not found");
 
             var newMessage = new Message
             {
                 Content = createMessageDto.Content,
                 SenderUsername = username,
+                SenderId = sender.Id,
                 ReceiverUsername = createMessageDto.ReceiverUsername,
+                ReceiverId = receiver.Id,
             };
+
+            messageRepository.AddMessage(newMessage);
+            return mapper.Map<MessageDto>(newMessage);
         }
     }
 }
