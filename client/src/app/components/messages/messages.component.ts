1c45ed9760ab9dceb1379860877fc2242121ada8
William Roberts <werobert@yahoo.com>
2025-01-06 14:55:23 -0500

    radio-btns-msgs

--- client/src/app/components/messages/messages.component.ts
+++ client/src/app/components/messages/messages.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MessagesService } from '../../services/messages.service';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-messages',
 	standalone: true,
-	imports: [],
+	imports: [ButtonsModule, FormsModule],
 	templateUrl: './messages.component.html',
 	styleUrl: './messages.component.css',
 })
@@ -16,17 +18,17 @@ export class MessagesComponent implements OnInit {
 	container = 'outbox';
 
 	ngOnInit(): void {
-		this.getMessages();
+		this.loadMessages();
 	}
 
-	getMessages(): void {
+	loadMessages(): void {
 		this.msgService.getMessages(this.pgNumber, this.pgSize, this.container);
 	}
 
 	onPageChanged(event: PageChangedEvent): void {
 		if (this.pgNumber !== event.page) {
 			this.pgNumber = event.page;
-			this.getMessages();
+			this.loadMessages();
 		}
 	}
 }
