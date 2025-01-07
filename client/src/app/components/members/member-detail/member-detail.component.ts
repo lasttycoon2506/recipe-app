6a34d16681243b9a00a8885d728cc769ef912438
William Roberts <werobert@yahoo.com>
2025-01-07 14:51:34 -0500

    tab-loading

--- client/src/app/components/members/member-detail/member-detail.component.ts
+++ client/src/app/components/members/member-detail/member-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { MemberService } from '../../../services/member.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../../models/member';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TabDirective, TabsModule } from 'ngx-bootstrap/tabs';
 import { DatePipe } from '@angular/common';
 import { MemberMessageComponent } from '../member-message/member-message.component';
+import { Message } from '../../../models/message';
+import { MessagesService } from '../../../services/messages.service';
 
 @Component({
 	selector: 'app-member-detail',
@@ -14,12 +16,28 @@ import { MemberMessageComponent } from '../member-message/member-message.compone
 	styleUrl: './member-detail.component.css',
 })
 export class MemberDetailComponent implements OnInit {
+	@ViewChild('memberTabs') memberTabs?: TabsModule;
 	private route = inject(ActivatedRoute);
 	private memberService = inject(MemberService);
+	private msgService = inject(MessagesService);
 	member?: Member;
+	activeTab?: TabDirective;
+	msgThread: Message[] = [];
 
 	ngOnInit(): void {
 		this.loadMember();
+	}
+
+	onActivateTab(data: Event): void {
+		this.activeTab = data;
+		if (
+			this.activeTab?.heading === 'Messages' &&
+			this.msgThread.length === 0
+		) {
+			this.msgService.getMessageThread(this.member!.username).subscribe({
+				next: (msgThread) => (this.msgThread = msgThread),
+			});
+		}
 	}
 
 	loadMember(): void {
