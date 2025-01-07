a816c5074d318510b4107309a1ca8a1b912d1c93
William Roberts <werobert@yahoo.com>
2025-01-07 13:59:27 -0500

    msg footer

--- client/src/app/components/members/member-message/member-message.component.ts
+++ client/src/app/components/members/member-message/member-message.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, input, OnInit } from '@angular/core';
 import { MessagesService } from '../../../services/messages.service';
 import { Message } from '../../../models/message';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-member-message',
 	standalone: true,
-	imports: [],
+	imports: [FormsModule],
 	templateUrl: './member-message.component.html',
 	styleUrl: './member-message.component.css',
 })
