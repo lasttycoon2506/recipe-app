69e778ab2faf56b2a3cac1f8975276801c78a06a
William Roberts <werobert@yahoo.com>
2025-01-08 12:56:56 -0500

    removed loadMember()

--- client/src/app/components/members/member-detail/member-detail.component.ts
+++ client/src/app/components/members/member-detail/member-detail.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import { MemberService } from '../../../services/member.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../../models/member';
 import { TabDirective, TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
@@ -55,13 +54,4 @@ export class MemberDetailComponent implements OnInit {
 			if (msgTab) msgTab.active = true;
 		}
 	}
-
-	// loadMember(): void {
-	// 	const username = this.route.snapshot.paramMap.get('username');
-	// 	if (!username) return;
-	// 	this.memberService.getMember(username).subscribe({
-	// 		next: (member) => (this.member = member),
-	// 		error: (err) => console.log(err),
-	// 	});
-	// }
 }
