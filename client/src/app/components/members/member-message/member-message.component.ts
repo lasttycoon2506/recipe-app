6759571ee92948cdef6d55038fe2905eaff5312d
William Roberts <werobert@yahoo.com>
2025-01-09 12:06:42 -0500

    msgs-comp. display

--- client/src/app/components/members/member-message/member-message.component.ts
+++ client/src/app/components/members/member-message/member-message.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, input, output, ViewChild } from '@angular/core';
 import { MessagesService } from '../../../services/messages.service';
 import { Message } from '../../../models/message';
 import { FormsModule, NgForm } from '@angular/forms';
+import { MemberService } from '../../../services/member.service';
+import { Member } from '../../../models/member';
 
 @Component({
 	selector: 'app-member-message',
@@ -13,10 +15,12 @@ import { FormsModule, NgForm } from '@angular/forms';
 export class MemberMessageComponent {
 	@ViewChild('msgForm') msgForm?: NgForm;
 	private msgService = inject(MessagesService);
+	memberService = inject(MemberService);
 	username = input<string>('');
 	msgThread = input<Message[] | null>(null);
 	msgContent = '';
 	newMsg = output<Message>();
+	member = input.required<Member>();
 
 	sendMsg(): void {
 		this.msgService
@@ -27,6 +31,4 @@ export class MemberMessageComponent {
 				},
 			});
 	}
-
-	deleteMsg() {}
 }
