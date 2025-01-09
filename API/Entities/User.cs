c35d43de9d1aee3494491e550151751e3adac45b
William Roberts <werobert@yahoo.com>
2025-01-09 14:09:20 -0500

    user identity entity upd.

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -1,11 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace API.Entities;
 
-public class User
+public class User : IdentityUser<int>
 {
-    public int Id { get; set; }
-    public required string UserName { get; set; }
-    public byte[] PasswordHash { get; set; } = [];
-    public byte[] PasswordSalt { get; set; } = [];
     public List<Photo> Photos { get; set; } = [];
     public DateOnly Created { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     public DateTime LastActive { get; set; } = DateTime.UtcNow;
@@ -15,4 +13,5 @@ public class User
     public List<UserLikes> WhoUserLikes { get; set; } = [];
     public List<Message> MessagesSent { get; set; } = [];
     public List<Message> MessagesReceived { get; set; } = [];
+    public ICollection<UserRole> UserRoles { get; set; } = [];
 }
