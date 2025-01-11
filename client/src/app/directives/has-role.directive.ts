467ab710bc1b25c9bf5bc61cd58442538a2ee87e
William Roberts <werobert@yahoo.com>
2025-01-11 11:40:42 -0500

    has-role directive

---
+++ client/src/app/directives/has-role.directive.ts
@@ -0,0 +1,25 @@
+import {
+	Directive,
+	inject,
+	OnInit,
+	TemplateRef,
+	ViewContainerRef,
+} from '@angular/core';
+import { AccountService } from '../services/account.service';
+
+@Directive({
+	selector: '[appHasRole]',
+	standalone: true,
+})
+export class HasRoleDirective implements OnInit {
+	appHasRole: string = '';
+	private accountService = inject(AccountService);
+	viewContainerRef = inject(ViewContainerRef);
+	templateRef = inject(TemplateRef);
+
+	ngOnInit(): void {
+		if (this.accountService.roles().includes(this.appHasRole))
+			this.viewContainerRef.createEmbeddedView(this.templateRef);
+		else this.viewContainerRef.clear();
+	}
+}
